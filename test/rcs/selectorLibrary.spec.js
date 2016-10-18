e2ac331af013bbce63d18a646ef6bed07b7007c0
JPeer264 <jan.oster94@gmail.com>
2016-10-18 23:28:25 +0200

    added tests

--- test/rcs/selectorLibrary.spec.js
+++ test/rcs/selectorLibrary.spec.js
@@ -4,12 +4,18 @@ const rcs    = require('../../lib/utils/rcs');
 const expect = require('chai').expect;
 
 describe('rcs selector library', () => {
+    beforeEach(() => {
+        // reset counter and selectors for tests
+        rcs.selectorLibrary.selectors = {};
+        rcs.nameGenerator.resetCountForTests();
+    });
+
     describe('set a new values', () => {
         it('should set a new value and get this value', done => {
             rcs.selectorLibrary.set('.test');
 
-            expect(rcs.selectorLibrary.get('.test')).to.equal('a1');
-            expect(rcs.selectorLibrary.get('test')).to.equal('a1');
+            expect(rcs.selectorLibrary.get('.test')).to.equal('e');
+            expect(rcs.selectorLibrary.get('test')).to.equal('e');
 
             done();
         });
@@ -27,7 +33,29 @@ describe('rcs selector library', () => {
 
             expect(setValueObject.type).to.equal('class');
             expect(setValueObject.selector).to.equal('.test');
-            expect(setValueObject.compressedSelector).to.equal('a1');
+            expect(setValueObject.compressedSelector).to.equal('e');
+
+            done();
+        });
+
+        it('should set values out of an array', done => {
+            rcs.selectorLibrary.set([
+                '.test',
+                '#id',
+                '.jp-selector'
+            ]);
+
+            // should be set
+            expect(rcs.selectorLibrary.get('.test')).to.equal('e');
+            expect(rcs.selectorLibrary.get('test')).to.equal('e');
+            expect(rcs.selectorLibrary.get('#id')).to.equal('t');
+            expect(rcs.selectorLibrary.get('id')).to.equal('t');
+            expect(rcs.selectorLibrary.get('.jp-selector')).to.equal('n');
+            expect(rcs.selectorLibrary.get('jp-selector')).to.equal('n');
+
+            // should not be set
+            expect(rcs.selectorLibrary.get('.not-set')).to.equal('.not-set');
+            expect(rcs.selectorLibrary.get('not-set')).to.equal('not-set');
 
             done();
         });
