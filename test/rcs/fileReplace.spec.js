3b23df167c4acb485b58f9f0d84045d427e34605
JPeer264 <jan.oster94@gmail.com>
2016-10-23 00:35:45 +0200

    added:  * tests  * .js testfiles with .txt ending  * .html testfiles updated var rewriting | options - cb

--- test/rcs/fileReplace.spec.js
+++ test/rcs/fileReplace.spec.js
@@ -5,18 +5,59 @@ const fs     = require('fs');
 const expect = require('chai').expect;
 
 describe('rcs file replace', () => {
-    describe('replaceCss', () => {
-        beforeEach(() => {
-            // reset counter and selectors for tests
-            rcs.selectorLibrary.selectors = {};
-            rcs.nameGenerator.resetCountForTests();
-        });
+    beforeEach(() => {
+        // reset counter and selectors for tests
+        rcs.selectorLibrary.selectors = {};
+        rcs.nameGenerator.resetCountForTests();
+    });
 
-        it('should replace css file and return modified selectors', done => {
-            rcs.fileReplace.replaceCss('test/files/fixtures/style.css', {}, (err, data) => {
+    describe('replaceCss', () => {
+        it('should return the modified css file', done => {
+            rcs.fileReplace.replaceCss('test/files/fixtures/style.css', (err, data) => {
                 expect(data).to.equal(fs.readFileSync('test/files/results/style.css', 'utf8'));
 
                 done();
+            });
+        });
+
+        it('should fail', done => {
+            rcs.fileReplace.replaceCss('non/exisiting/path.css', err => {
+                expect(err).to.be.an('object');
+                expect(err.error).to.equal('ENOENT');
+
+                done();
+            });
+        });
+    });
+
+    describe('replace any file', () => {
+        it('should fail', done => {
+            rcs.fileReplace.replace('non/exisiting/path.css', err => {
+                expect(err).to.be.an('object');
+                expect(err.error).to.equal('ENOENT');
+
+                done();
+            });
+        });
+
+        it('should return the modified html file', done => {
+            rcs.fileReplace.replaceCss('test/files/fixtures/style.css', (err, data) => {
+                rcs.fileReplace.replace('test/files/fixtures/index.html', (err, data) => {
+                    expect(data).to.equal(fs.readFileSync('test/files/results/index.html', 'utf8'));
+
+                    done();
+                });
+            });
+        });
+
+        it('should return the modified js file', done => {
+            // `js` file imported as `txt` to avoid having mocha-phantomjs
+            rcs.fileReplace.replaceCss('test/files/fixtures/style.css', (err, data) => {
+                rcs.fileReplace.replace('test/files/fixtures/main.txt', (err, data) => {
+                    expect(data).to.equal(fs.readFileSync('test/files/results/main.txt', 'utf8'));
+
+                    done();
+                });
             });
         });
     });
