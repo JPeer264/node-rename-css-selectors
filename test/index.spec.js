1347c4ba2354f367934b6367127e347a1b56a12b
JPeer264 <jan.oster94@gmail.com>
2017-02-07 12:45:39 +0100

    improve coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -107,6 +107,19 @@ describe('app.js', () => {
             });
         });
 
+        it('should process css files without options', done => {
+            app.processCss(fixturesCwd + '/**/*.css', (err, data) => {
+                let newFile       = fs.readFileSync('./rcs/' + fixturesCwd + '/style.css', 'utf8');
+                let expectedFile  = fs.readFileSync(resultsCwd + '/style.css', 'utf8');
+
+                expect(err).to.not.exist;
+                expect(newFile).to.equal(expectedFile);
+
+                fs.removeSync('./rcs');
+
+                done();
+            });
+        });
 
         it('should process css files and flatten the directories', done => {
             app.process('**/*.css', {
@@ -194,7 +207,7 @@ describe('app.js', () => {
             });
         });
 
-        it('should create the normal library file', done => {
+        it('should create the normal mapping file', done => {
             app.generateMapping(testCwd, (err, data) => {
                 const cssMapping = json.readToObjSync(testCwd + '/renaming_map.json', 'utf8');
 
@@ -207,23 +220,23 @@ describe('app.js', () => {
             });
         });
 
-        it('should create the minified library file', done => {
+        it('should create the minified mapping file', done => {
             app.generateMapping(testCwd, {
                 cssMapping: false,
                 cssMappingMin: true
             }, (err, data) => {
-                const cssMapping = json.readToObjSync(testCwd + '/renaming_map_min.json', 'utf8');
+                const cssMappingMin = json.readToObjSync(testCwd + '/renaming_map_min.json', 'utf8');
 
                 expect(err).to.not.exist;
-                expect(cssMapping['.a']).to.equal('jp-block');
-                expect(cssMapping['.b']).to.equal('jp-block__element');
+                expect(cssMappingMin['.a']).to.equal('jp-block');
+                expect(cssMappingMin['.b']).to.equal('jp-block__element');
 
                 done();
 
             });
         });
 
-        it('should create the extended normal library file', done => {
+        it('should create the extended normal mapping file', done => {
             app.generateMapping(testCwd, {
                 extended: true
             }, (err, data) => {
@@ -239,7 +252,7 @@ describe('app.js', () => {
             });
         });
 
-        it('should create the minified library file', done => {
+        it('should create the minified mapping file', done => {
             app.generateMapping(testCwd, {
                 cssMapping: false,
                 cssMappingMin: true,
@@ -256,6 +269,51 @@ describe('app.js', () => {
 
             });
         });
+
+        it('should create the custom names minified mapping file', done => {
+            app.generateMapping(testCwd, {
+                cssMapping: 'custom-name'
+            }, (err, data) => {
+                const cssMapping = json.readToObjSync(testCwd + '/custom-name.json', 'utf8');
+
+                expect(err).to.not.exist;
+                expect(cssMapping['.jp-block']).to.equal('a');
+                expect(cssMapping['.jp-block__element']).to.equal('b');
+
+                done();
+
+            });
+        });
+
+        it('should create the minified mapping file', done => {
+            app.generateMapping(testCwd, {
+                cssMappingMin: 'custom-name'
+            }, (err, data) => {
+                const cssMappingMin = json.readToObjSync(testCwd + '/custom-name.json', 'utf8');
+
+                expect(err).to.not.exist;
+                expect(cssMappingMin['.a']).to.equal('jp-block');
+                expect(cssMappingMin['.b']).to.equal('jp-block__element');
+
+                done();
+
+            });
+        });
+
+        it('should create the minified mapping js file', done => {
+            app.generateMapping(testCwd, {
+                json: false
+            }, (err, data) => {
+                const cssMapping = fs.readFileSync(testCwd + '/renaming_map.js', 'utf8');
+
+                expect(err).to.not.exist;
+                expect(cssMapping).to.match(/var CSS_NAME_MAPPING = {/);
+
+                done();
+
+            });
+        });
+
 
         it('should overwrite mapping files', done => {
             app.generateMapping(testCwd, (err, data) => {
