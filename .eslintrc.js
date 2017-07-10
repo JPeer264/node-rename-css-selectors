e9805bcdd522205ecebf554a75bd9831138c1de5
JPeer264 <jan.oster94@gmail.com>
2017-07-10 22:45:50 +0200

    Chore: add eslint to workflow

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        // disabled until babel rewrite
+        "strict": 1,
+        "no-param-reassign": 1,
+        "max-len": 1,
+        "no-shadow": 1,
+    }
+};
