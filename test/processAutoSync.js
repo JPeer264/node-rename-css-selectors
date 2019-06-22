13610be0d74b6341b19073d07b67554b218ef973
JPeer264 <jan.oster94@gmail.com>
2019-06-22 18:05:16 +0200

    Fix: process fill html for sync variant

--- test/processAutoSync.js
+++ test/processAutoSync.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import path from 'path';
 import fs from 'fs-extra';
 import rcsCore from 'rcs-core';
+import { minify } from 'html-minifier';
 
 import rcs from '../';
 
@@ -38,4 +39,25 @@ test('should process all files synchronously', (t) => {
   expectedFile = fs.readFileSync(path.join(resultsCwd, '/css/style.css'), 'utf8');
 
   t.is(newFile, expectedFile);
+});
+
+test('should fillLibraries from html and css | issue #38', (t) => {
+  rcs.process.autoSync(['**/*.{js,html}', 'css/style.css'], {
+    newPath: testCwd,
+    cwd: fixturesCwd,
+  });
+
+  const newFile = fs.readFileSync(path.join(testCwd, '/html/index-with-style.html'), 'utf8');
+  const newFile2 = fs.readFileSync(path.join(testCwd, '/css/style.css'), 'utf8');
+  const expectedFile = fs.readFileSync(path.join(resultsCwd, '/html/index-with-style.html'), 'utf8');
+  const expectedFile2 = fs.readFileSync(path.join(resultsCwd, '/css/style.css'), 'utf8');
+
+  t.is(
+    minify(newFile, { collapseWhitespace: true }),
+    minify(expectedFile, { collapseWhitespace: true }),
+  );
+  t.is(
+    minify(newFile2, { collapseWhitespace: true }),
+    minify(expectedFile2, { collapseWhitespace: true }),
+  );
 });
